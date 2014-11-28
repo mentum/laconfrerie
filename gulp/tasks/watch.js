1c3f925e1f9e29f7cd9e08c28b85d95e095abc1c
Olivier Sylvain <g.oliviersylvain@gmail.com>
2014-11-28 15:00:39 -0500

    font

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 
 module.exports = gulp.task('watch', function () {
     gulp.watch([configs.paths.styles + '**/*.less'], ['styles']);
-    gulp.watch(configs.paths.index, ['views']);
+    gulp.watch(configs.paths.index, ['static']);
     gulp.watch([configs.paths.scripts], ['scripts']);
 });
